ad4fb58131f36ca14cc9ef499114466a1718050d
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-06-21 14:14:10 -0300

    ❌ remove: imports não usados

--- src/main/java/br/com/pizzutti/chatws/component/ChatWebSocketHandlerComponent.java
+++ src/main/java/br/com/pizzutti/chatws/component/ChatWebSocketHandlerComponent.java
@@ -1,21 +1,11 @@
 package br.com.pizzutti.chatws.component;
 
-import br.com.pizzutti.chatws.dto.MessageDto;
 import br.com.pizzutti.chatws.facade.WebSocketFacade;
-import br.com.pizzutti.chatws.service.TimeService;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.CloseStatus;
 import org.springframework.web.socket.TextMessage;
 import org.springframework.web.socket.WebSocketSession;
 import org.springframework.web.socket.handler.TextWebSocketHandler;
-
-import java.io.IOException;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
 
 @Component
 public class ChatWebSocketHandlerComponent extends TextWebSocketHandler {
