17b3d77962ea66a0bbea3e1b04a222e343bacfc5
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-06-22 10:43:55 -0300

    ✨ feat: melhora nas mensagens de UserCreateDto

--- src/main/java/br/com/pizzutti/chatws/dto/UserCreateDto.java
+++ src/main/java/br/com/pizzutti/chatws/dto/UserCreateDto.java
@@ -6,19 +6,19 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserCreateDto(
-        @NotBlank
-        @Size(min = 36, max = 36, message = "O totem deve ter exatamente 36 caracteres")
+        @NotBlank(message = "totem não pode estar vazio")
+        @Size(min = 36, max = 36, message = "totem deve ter exatamente 36 caracteres")
         String totem,
 
-        @NotBlank
+        @NotBlank(message = "e-mail não pode estar vazio")
         @Email(message = "E-mail inválido!")
         String login,
 
-        @NotBlank
-        @Size(min = 3, max = 20, message = "O nick deve ter entre 3 e 20 caracteres")
+        @NotBlank(message = "nick não pode estar vazio")
+        @Size(min = 3, max = 20, message = "nick deve ter entre 3 e 20 caracteres")
         String nickname,
 
-        @NotBlank(message = "Senha é obrigatória")
+        @NotBlank(message = "senha não pode estar vazia")
         @Pattern(
             regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*#?&^_-])[A-Za-z\\d@$!%*#?&^_-]{8,}$",
             message = "A senha deve ter no mínimo 8 caracteres, com letras maiúsculas, minúsculas, números e símbolos"
