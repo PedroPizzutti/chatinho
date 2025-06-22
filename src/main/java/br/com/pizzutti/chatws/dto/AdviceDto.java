5379d3721801996b2eac6732c5117fed4570acec
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-06-22 10:44:10 -0300

    ✨ feat: lista de erros e código no advice dto

--- src/main/java/br/com/pizzutti/chatws/dto/AdviceDto.java
+++ src/main/java/br/com/pizzutti/chatws/dto/AdviceDto.java
@@ -1,13 +1,16 @@
 package br.com.pizzutti.chatws.dto;
 
+import br.com.pizzutti.chatws.enums.AdviceEnum;
 import lombok.Builder;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Builder
 public record AdviceDto(
         LocalDateTime timestamp,
         Integer status,
-        String error,
+        List<String> errors,
+        AdviceEnum code,
         String path) {
 }
