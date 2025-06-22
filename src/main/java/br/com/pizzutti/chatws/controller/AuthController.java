9a0d687ea9937d2a817f9a817b005625064c5219
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-06-22 10:44:30 -0300

    📖 doc: melhoado swagger de AuthController

--- src/main/java/br/com/pizzutti/chatws/controller/AuthController.java
+++ src/main/java/br/com/pizzutti/chatws/controller/AuthController.java
@@ -2,6 +2,10 @@ package br.com.pizzutti.chatws.controller;
 
 import br.com.pizzutti.chatws.dto.*;
 import br.com.pizzutti.chatws.facade.UserFacade;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -20,12 +24,24 @@ public class AuthController {
     }
 
     @PostMapping("/login")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", content = @Content(schema = @Schema(implementation = UserLoggedDto.class))),
+        @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = AdviceDto.class))),
+        @ApiResponse(responseCode = "401", content = @Content(schema = @Schema(implementation = AdviceDto.class))),
+        @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = AdviceDto.class)))
+    })
     public ResponseEntity<UserLoggedDto> login(@RequestBody UserLoginDto userLoginDto) {
         var userLoggedDto = this.userFacade.loginApi(userLoginDto);
         return ResponseEntity.status(201).body(userLoggedDto);
     }
 
     @PostMapping("/create-user")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "201", content = @Content(schema = @Schema(implementation = UserCreatedDto.class))),
+        @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = AdviceDto.class))),
+        @ApiResponse(responseCode = "403", content = @Content(schema = @Schema(implementation = AdviceDto.class))),
+        @ApiResponse(responseCode = "500", content = @Content(schema = @Schema(implementation = AdviceDto.class)))
+    })
     public ResponseEntity<UserCreatedDto> createUser(@RequestBody @Valid UserCreateDto userCreateDto) {
         var user = this.userFacade.createUser(userCreateDto);
         return ResponseEntity.status(201).body(user);
