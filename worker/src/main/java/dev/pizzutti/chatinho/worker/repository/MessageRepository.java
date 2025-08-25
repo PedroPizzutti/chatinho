a681028a293acabb5cd877a3e47d0b8bd77d726e
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-08-24 21:55:28 -0300

    ♻️ refac: restaurada estrutura de pastas do repositório

--- worker/src/main/java/dev/pizzutti/chatinho/worker/repository/MessageRepository.java
+++
@@ -1,7 +0,0 @@
-package dev.pizzutti.chatinho.worker.repository;
-
-import dev.pizzutti.chatinho.worker.model.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MessageRepository extends JpaRepository<Message, Long> {
-}
