648391647a988005971e7da0a0f557fde5021dfd
Pedro Pizzutti <pedropizzutti@gmail.com>
2025-11-08 17:33:22 -0300

    ✨ feat: add .env para segurança

--- src/main/java/br/com/pizzutti/chatinho/api/domain/invite/InviteFacade.java
+++ src/main/java/br/com/pizzutti/chatinho/api/domain/invite/InviteFacade.java
@@ -14,7 +14,6 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
-import java.util.Objects;
 import java.util.Optional;
 
 @Service
